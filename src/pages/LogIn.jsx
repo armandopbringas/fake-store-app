1d4446b5181cb461b4e398b9a3edda51524c28f4
armandopbringas <bringas.armandop@gmail.com.com>
2023-10-14 16:49:43 -0600

    style: :building_construction: addin UI styles

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { supabase } from "../../client";
+import { TEInput, TERipple } from "tw-elements-react";
 
 const LogIn = ({ setToken }) => {
   let navigate = useNavigate();
@@ -35,16 +36,30 @@ const LogIn = ({ setToken }) => {
   }
 
   return (
-    <div>
+    <div  className="block max-w-sm rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
       <form onSubmit={handleSubmit}>
-        <input placeholder="Email" name="email" onChange={handleChange} />
-        <input
-          placeholder="Password"
-          name="password"
-          type="password"
+        <TEInput
+          type="email"
+          name="email"
+          label="Email address"
           onChange={handleChange}
         />
-        <button type="submit">Submit</button>
+        <TEInput
+          type="password"
+          name="password"
+          label="Password"
+          className="mt-12 mb-6"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <TERipple rippleColor="light">
+          <button
+            type="submit"
+            className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+          >
+            Log In
+          </button>
+        </TERipple>
       </form>
       Don't have an account? <Link to="/signup">Sign Up</Link>
     </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../../client";
+import { TEInput, TERipple } from "tw-elements-react";
 
 const SignUp = () => {
   let navigate = useNavigate();
@@ -37,26 +38,30 @@ const SignUp = () => {
   };
 
   return (
-    <div>
+    <div  className="block max-w-sm rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
       <h2>Sign in</h2>
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="fullname">Full Name</label>
-          <input
+          <TEInput
+            required
             type="text"
             id="fullname"
             name="fullname"
+            className="mb-6"
+            label="First name"
             value={formData.fullname}
             onChange={handleChange}
-            required
           />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input
+          <TEInput
             type="email"
             id="email"
             name="email"
+            label="First name"
+            className="mb-6"
             value={formData.email}
             onChange={handleChange}
             required
@@ -64,16 +69,23 @@ const SignUp = () => {
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input
+          <TEInput
             type="password"
             id="password"
             name="password"
+            label="Password"
+            className="mb-6"
             value={formData.password}
             onChange={handleChange}
             required
           />
         </div>
-        <button type="submit">Sign in</button>
+        <button 
+          type="submit"
+          className="block w-full rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]]"
+        >
+          Sign in
+        </button>
       </form>
     </div>
   );
