1a15dc3dcbb7792646292007ea0da8ca805865fb
armandopbringas <bringas.armandop@gmail.com.com>
2023-10-14 15:11:07 -0600

    fix: :bug: fixing bugs in login flow

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,13 @@
-const Layout = ({ children }) => {
+import Nav from "../Nav/Nav";
+import NavLoged from "../NavLoged/NavLoged";
+
+const Layout = ({ children, token, setToken }) => {
   return (
     <div>
+      {!token ? <Nav /> : <NavLoged setToken={setToken} />}
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,38 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Landing from '../pages/Landing';
-import SignUp from '../pages/SignUp';
-import LogIn from '../pages/LogIn';
-import Home from '../pages/Home';
-import NavLoged from '../components/NavLoged/NavLoged';
-import Nav from '../components/Nav/Nav';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Landing from "../pages/Landing";
+import SignUp from "../pages/SignUp";
+import LogIn from "../pages/LogIn";
+import Home from "../pages/Home";
+import Layout from "../components/Layout/Layout";
 
 const AppRouter = () => {
   const [token, setToken] = useState(false);
-  
+
   useEffect(() => {
-    if (sessionStorage.getItem('token')) {
-      let data = JSON.parse(sessionStorage.getItem('token'));
+    if (sessionStorage.getItem("token")) {
+      let data = JSON.parse(sessionStorage.getItem("token"));
       setToken(data);
     }
   }, []);
 
+  const PrivateRoute = ({ element, token }) => {
+    return token ? element : <Navigate to="/login" />;
+  };
+
   return (
     <BrowserRouter>
-      <div>
-        {!token ? (<Nav />) : (<NavLoged setToken={setToken} />)}
-      </div>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route
-          path="/login"
-          element={<LogIn setToken={setToken} />}
-        />
-        {token && (<Route path="/home" element={<Home token={token} />} />)}
-      </Routes>
+      <Layout token={token} setToken={setToken}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn setToken={setToken} />} />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute element={<Home token={token} />} token={token} />
+            }
+          />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
-}
+};
 
 export default AppRouter;
